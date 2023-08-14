d55e859fa50f1eb4951764bc95bf92161b8e87d1
Mohan <101608624+MohanDogiparthi@users.noreply.github.com>
2023-08-15 00:50:57 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ if [ $? —eq 0 ]
 then
 echo "Validation Sucessfull.lLets Build The Image."
 packer build packer.json 2>$1 | tee output.txt
-tail -2 output.txt | head -2 | awk 'match(S0, /ami-.*/) { print substr(S0, RSTART, RLENGTH)
+tail -2 output.txt | head -2 | awk 'match(S0, /ami-.*/) { print substr(S0, RSTART, RLENGTH) }' > ami.txt
 AMIID=$(cat ami.txt)
 echo "variable imagename { default = $AMIID }" >> variables.tf
 terraform init
